9ed01bea9ff8fb1ba5bba9a17c0ec0724548817c
Giovanni Panzetta <giodevcoding@gmail.com>
2023-09-14 20:40:05 -0500

    initial aviality conversion

--- dist/jhipster-fix.js
+++ dist/jhipster-fix.js
@@ -1,8 +1,35 @@
 #! /usr/bin/env node
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs = __importStar(require("node:fs/promises"));
+const readline = __importStar(require("node:readline"));
 const glob_1 = require("glob");
 const commander_1 = require("commander");
+const node_fs_1 = require("node:fs");
+/** The main function that is executed at the end of the file */
 async function main() {
     const program = new commander_1.Command();
     program
@@ -10,11 +37,73 @@ async function main() {
         .description("A tool for fixing JHipster generated files with more up-to-date packages")
         .parse(process.argv);
     const sourceFolder = program.args.length > 0 ? program.args[0] : process.cwd() + "/src/";
-    console.log(await getJSFiles(sourceFolder));
+    const jsFilePaths = await getJSFilePaths(sourceFolder);
+    updateJSFiles(jsFilePaths);
 }
-async function getJSFiles(srcFolder) {
-    const files = (await (0, glob_1.glob)(srcFolder + "/**/*.{js,jsx,ts,tsx}"));
+/**
+ * Get all file paths that need to be changed
+ * */
+async function getJSFilePaths(srcFolder) {
+    const files = await (0, glob_1.glob)(srcFolder + "/**/*.{js,jsx,ts,tsx}");
     return files;
+}
+async function updateJSFiles(filePaths) {
+    for (const path of filePaths) {
+        try {
+            const lines = await getFileLines(path);
+            updateAvailityPackages(path, lines);
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+}
+async function getFileLines(path) {
+    const linesInterface = readline.createInterface({
+        input: (0, node_fs_1.createReadStream)(path),
+        crlfDelay: Infinity,
+    });
+    const lines = [];
+    for await (const line of linesInterface) {
+        lines.push(line);
+    }
+    return lines;
+}
+/**
+ * Updates availity-reactstrap-validation to @availity/form and the imported components.
+ *
+ * @throws NodeJS.ErrnoException
+ */
+function updateAvailityPackages(path, lines) {
+    const maxLines = Math.min(lines.length, 20);
+    let availityLineIndex = -1;
+    for (let i = 0; i < maxLines; i++) {
+        const line = lines[i];
+        if (line.includes("availity-reactstrap-validation")) {
+            availityLineIndex = i;
+            break;
+        }
+    }
+    if (availityLineIndex === -1) {
+        return;
+    }
+    fs.readFile(path)
+        .then((data) => {
+        const newData = data
+            .toString()
+            .replaceAll(/availity-reactstrap-validation/g, "@availity/form")
+            .replaceAll(/AvFeedback/g, "Feedback")
+            .replaceAll(/AvForm/g, "Form")
+            .replaceAll(/AvGroup/g, "FormGroup")
+            .replaceAll(/AvInput/g, "Input")
+            .replaceAll(/AvField/g, "Field");
+        fs.writeFile(path, newData).then(() => {
+            console.log("Updated availity-reactstrap-validation to @availity/form in:", path);
+        });
+    })
+        .catch((err) => {
+        throw err;
+    });
 }
 main();
 //# sourceMappingURL=jhipster-fix.js.map
--- src/jhipster-fix.ts
+++ src/jhipster-fix.ts
@@ -1,27 +1,108 @@
 #! /usr/bin/env node
+import * as fs from "node:fs/promises";
+import * as readline from "node:readline";
+import { glob } from "glob";
+import { Command } from "commander";
+import { createReadStream } from "node:fs";
 
-import { glob } from "glob"
-import { Command } from "commander"
-
+/** The main function that is executed at the end of the file */
 async function main() {
   const program = new Command();
 
   program
     .version("1.0.0")
-    .description("A tool for fixing JHipster generated files with more up-to-date packages")
-    .parse(process.argv)
+    .description(
+      "A tool for fixing JHipster generated files with more up-to-date packages",
+    )
+    .parse(process.argv);
 
-  const sourceFolder = program.args.length > 0 ? program.args[0] : process.cwd() + "/src/"
+  const sourceFolder =
+    program.args.length > 0 ? program.args[0] : process.cwd() + "/src/";
 
-  console.log(await getJSFiles(sourceFolder))
+  const jsFilePaths = await getJSFilePaths(sourceFolder);
+  updateJSFiles(jsFilePaths);
 }
 
-async function getJSFiles (srcFolder: string) {
-
-  const files = (await glob(srcFolder + "/**/*.{js,jsx,ts,tsx}"))  
+/**
+ * Get all file paths that need to be changed
+ * */
+async function getJSFilePaths(srcFolder: string) {
+  const files = await glob(srcFolder + "/**/*.{js,jsx,ts,tsx}");
   return files;
 }
 
+async function updateJSFiles(filePaths: string[]) {
+  for (const path of filePaths) {
+    try {
+      const lines = await getFileLines(path);
+
+      updateAvailityPackages(path, lines);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
+
+async function getFileLines(path: string) {
+  const linesInterface = readline.createInterface({
+    input: createReadStream(path),
+    crlfDelay: Infinity,
+  });
+  const lines: string[] = [];
+
+  for await (const line of linesInterface) {
+    lines.push(line);
+  }
+
+  return lines as readonly string[];
+}
+
+/**
+ * Updates availity-reactstrap-validation to @availity/form and the imported components.
+ *
+ * @throws NodeJS.ErrnoException
+ */
+function updateAvailityPackages(
+  path: string,
+  lines: readonly string[],
+): void | never {
+  const maxLines = Math.min(lines.length, 20);
+
+  let availityLineIndex = -1;
+
+  for (let i = 0; i < maxLines; i++) {
+    const line = lines[i];
+    if (line.includes("availity-reactstrap-validation")) {
+      availityLineIndex = i;
+      break;
+    }
+  }
+
+  if (availityLineIndex === -1) {
+    return;
+  }
+
+  fs.readFile(path)
+    .then((data) => {
+      const newData = data
+        .toString()
+        .replaceAll(/availity-reactstrap-validation/g, "@availity/form")
+        .replaceAll(/AvFeedback/g, "Feedback")
+        .replaceAll(/AvForm/g, "Form")
+        .replaceAll(/AvGroup/g, "FormGroup")
+        .replaceAll(/AvInput/g, "Input")
+        .replaceAll(/AvField/g, "Field");
+
+      fs.writeFile(path, newData).then(() => {
+        console.log(
+          "Updated availity-reactstrap-validation to @availity/form in:",
+          path,
+        );
+      });
+    })
+    .catch((err: NodeJS.ErrnoException) => {
+      throw err;
+    });
+}
+
 main();
-
-
